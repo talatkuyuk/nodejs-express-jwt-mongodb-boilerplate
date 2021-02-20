0966f73647fb3906c2cef30eb2f9a63a5257dec1
talatkuyuk <talatkuyuk@gmail.com>
2021-02-20 23:30:43 +0300

    added parsing request body

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
 var express = require('express');
+var path = require('path');
+
 var app = express();
+
+
+// parse JSON (parse application/json)
+app.use(express.json());
+
+// parse urlencoded bodies (parse application/x-www-form-urlencoded)
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res, next) => {
     res.send("Hello");
