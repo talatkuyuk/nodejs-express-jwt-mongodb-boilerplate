b2b14826c1bb66d2906dfafb2755161efc7e955a
“talatkuyuk” <talatkuyuk@gmail.com>
2021-10-05 23:08:47 +0300

    updated swagger options, added docs for auth and authuser routes

--- src/swagger/swagger.options.js
+++ src/swagger/swagger.options.js
@@ -1,27 +1,42 @@
 const options = {
 	swaggerDefinition: {
-		openapi: "3.0.0",
+		openapi: "3.0.2",
 		info: {
 			title: "API Documentation",
-			version: "1.0.0",
 			description: "for github.com/talatkuyuk/nodejs-express-jwt-mongodb-boilerplate",
-		},
-		license: {
-			name: 'MIT',
-			url: 'https://github.com/talatkuyuk/nodejs-express-jwt-mongodb-boilerplate/blob/master/LICENSE',
-		},
-		contact: {
-			name: "Talat Kuyuk",
-			url: "https://github.com/talatkuyuk/nodejs-express-jwt-mongodb-boilerplate",
-			email: "talatkuyuk@gmail.com",
+			contact: {
+				name: "Talat Kuyuk",
+				url: "https://github.com/talatkuyuk/nodejs-express-jwt-mongodb-boilerplate",
+				email: "talatkuyuk@gmail.com",
+			},
+			license: {
+				name: 'MIT',
+				url: 'https://github.com/talatkuyuk/nodejs-express-jwt-mongodb-boilerplate/blob/master/LICENSE',
+			},
+			version: "1.0.0",
 		},
 		servers: [
 			{
 				url: "https://localhost:8443",
+				description: 'Secure development server',
+			},
+			{
+				url: "http://localhost:3000",
+				description: 'Non-Secure development server',
 			},
 		],
+		tags: [
+			{
+				name: "Auth",
+				description: 'Authentication',
+			},
+			{
+				name: "AuthUser",
+				description: 'Manuplates the AuthUsers',
+			},
+		]
 	},
-	apis: [ `${__dirname}/*.js` ], 
+	apis: [ `${__dirname}/*.yaml`, `${__dirname}/*.js` ], 
 };
 
 module.exports = {
