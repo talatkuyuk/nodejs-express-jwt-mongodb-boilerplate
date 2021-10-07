61442fb31109ff1711afbaa36238830ae69618ab
“talatkuyuk” <talatkuyuk@gmail.com>
2021-10-07 17:42:53 +0300

    updated morgan logger

--- src/core/morgan.js
+++ src/core/morgan.js
@@ -1,14 +1,15 @@
 const morgan = require('morgan');
+
 const config = require('../config');
 const logger = require('./logger');
 
 // it tokenizes errorMessage in res.locals as message. It is set via errorHandler in error.js
-morgan.token('message', (req, res) => res.locals.errorMessage || '');
-morgan.token('user', (req, res) => req?.user?.email || '');
+morgan.token('user', (req, res) => req?.user?.email || 'anonymous');
+morgan.token('error', (req, res) => res.locals.error || '');
 
 const getIpFormat = () => (config.env === 'production' ? ':remote-addr - ' : '');
-const successResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms user::user`;
-const errorResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms  user::user - message: :message`;
+const successResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms - user: :user`;
+const errorResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms - user: :user - :error`;
 
 const successHandler = morgan(successResponseFormat, {
   skip: (req, res) => res.statusCode >= 400,
