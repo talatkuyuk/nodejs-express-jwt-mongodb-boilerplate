ac01ed23aa5e73765d8e2f7126dda99177f68f62
“talatkuyuk” <talatkuyuk@gmail.com>
2021-10-15 00:31:20 +0300

    added try-catch block to all controllers

--- src/controllers/authuser.controller.js
+++ src/controllers/authuser.controller.js
@@ -1,10 +1,12 @@
 const httpStatus = require('http-status');
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler');
+
+const { locateError } = require('../utils/ApiError');
 
 // SERVICE DEPENDENCY
 const {
-	authService, 		// addAuthUser
 	authuserService, 	// getAuthUser, getAuthUsers, deleteAuthUser, changePassword, toggleAbility
+	authService, 		// addAuthUser
 	tokenService, 		// deleteAuthUser
 	joinedService,		// getAuthUsersJoined
 } = require('../services');
@@ -12,81 +14,115 @@ const {
 
 
 const addAuthUser = asyncHandler(async (req, res) => {
-	const { email, password } = req.body;
-
-	const authuser = await authService.signupWithEmailAndPassword(email, password);
-
-	// no need to generate tokens since the user is going to login self
-
-	res.status(httpStatus.CREATED).send({ user: authuser.filter() });
+	try {
+		const { email, password } = req.body;
+	
+		const authuser = await authService.signupWithEmailAndPassword(email, password);
+	
+		// no need to generate tokens since the user is going to login self
+	
+		res.status(httpStatus.CREATED).send({ user: authuser.filter() });
+		
+	} catch (error) {
+		throw locateError(error, "AuthUserController : addAuthUser");
+	}
 });
 
 
 
 const getAuthUser = asyncHandler(async (req, res) => {
-	const id = req.params.id;
-
-	// the service checks the param id refers any valid authuser
-	const authuser = await authuserService.getAuthUserById(id);
-
-	res.status(httpStatus.OK).send(authuser.filter());
+	try {
+		const id = req.params.id;
+	
+		// the service checks the param id refers any valid authuser
+		const authuser = await authuserService.getAuthUserById(id);
+	
+		res.status(httpStatus.OK).send(authuser.filter());
+		
+	} catch (error) {
+		throw locateError(error, "AuthUserController : getAuthUser");
+	}
 });
 
 
 
 const getAuthUsers = asyncHandler(async (req, res) => {
-	const query = req.query;
-
-	const result = await authuserService.getAuthUsers(query);
+	try {
+		const query = req.query;
 	
-	res.status(httpStatus.OK).send(result);
+		const result = await authuserService.getAuthUsers(query);
+		
+		res.status(httpStatus.OK).send(result);
+		
+	} catch (error) {
+		throw locateError(error, "AuthUserController : getAuthUsers");
+	}
 });
 
 
 
 const getAuthUsersJoined = asyncHandler(async (req, res) => {
-	const query = req.query;
-
-	const result = await joinedService.getAuthUsersJoined(query);
+	try {
+		const query = req.query;
 	
-	res.status(httpStatus.OK).send(result);
+		const result = await joinedService.getAuthUsersJoined(query);
+		
+		res.status(httpStatus.OK).send(result);
+		
+	} catch (error) {
+		throw locateError(error, "AuthUserController : getAuthUsersJoined");
+	}
 });
 
 
 
 const changePassword = asyncHandler(async (req, res) => {
-	const newPassword = req.body.password;
-	const authuser = req.user;
-
-	await authuserService.changePassword(authuser, newPassword);
-
-	res.status(httpStatus.NO_CONTENT).send();
+	try {
+		const newPassword = req.body.password;
+		const authuser = req.user;
+	
+		await authuserService.changePassword(authuser, newPassword);
+	
+		res.status(httpStatus.NO_CONTENT).send();
+		
+	} catch (error) {
+		throw locateError(error, "AuthUserController : changePassword");
+	}
 });
 
 
 
 const toggleAbility = asyncHandler(async (req, res) => {
-	const id = req.params.id;
-
-	// the service checks the param id refers any valid authuser
-	await authuserService.toggleAbility(id);
-  
-	res.status(httpStatus.NO_CONTENT).send();
+	try {
+		const id = req.params.id;
+	
+		// the service checks the param id refers any valid authuser
+		await authuserService.toggleAbility(id);
+	  
+		res.status(httpStatus.NO_CONTENT).send();
+		
+	} catch (error) {
+		throw locateError(error, "AuthUserController : toggleAbility");
+	}
 });
 
 
 
-// depend on tokenService because of token remove operation
 const deleteAuthUser = asyncHandler(async (req, res) => {
-	const id = req.params.id;
-
-	// the service checks the param id refers any valid authuser
-	await authuserService.deleteAuthUser(id);
+	try {
+		const id = req.params.id;
 	
-	// delete all tokens,
-	await tokenService.removeTokens({ user: id });
-
-	res.status(httpStatus.NO_CONTENT).send();
+		// the service checks the param id refers any valid authuser
+		await authuserService.deleteAuthUser(id);
+		
+		// delete all tokens,
+		await tokenService.removeTokens({ user: id });
+	
+		res.status(httpStatus.NO_CONTENT).send();
+		
+	} catch (error) {
+		throw locateError(error, "AuthUserController : deleteAuthUser");
+	}
 });
 
 
