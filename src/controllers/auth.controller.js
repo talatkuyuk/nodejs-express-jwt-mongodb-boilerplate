916486d261676945c1dd6d5a67c312902603dfa7
talatkuyuk <talatkuyuk@gmail.com>
2021-10-15 00:31:20 +0300

    added try-catch block to all controllers

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,138 +1,190 @@
 const httpStatus = require('http-status');
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler');
+
+const { locateError } = require('../utils/ApiError');
 
 // SERVICE DEPENDENCY
 const { 
-	authService,
-	authuserService,      //getAuthUser: forgotPassword
-	tokenService,   //generate token(s): forgotPassword, sendVerificationEmail, signup, login, refreshTokens
-	emailService,          //send email: forgotPassword, sendVerificationEmail
+	authService,			// all
+	tokenService,   		// forgotPassword, sendVerificationEmail, signup, login, refreshTokens
+	emailService,          	// forgotPassword, sendVerificationEmail
+	authuserService,      	// forgotPassword
 } = require('../services');
 
 
 
 const signup = asyncHandler(async (req, res) => {
-	const { email, password } = req.body;
-	const userAgent = req.useragent.source;
-
-	const authuser = await authService.signupWithEmailAndPassword(email, password);
-	const tokens = await tokenService.generateAuthTokens(authuser.id, userAgent);
-
-	req.user = authuser; // for morgan logger to tokenize it as user
-
-	res.status(httpStatus.CREATED).send({ user: authuser.filter(), tokens });
+	try {
+		const { email, password } = req.body;
+		const userAgent = req.useragent.source;
+	
+		const authuser = await authService.signupWithEmailAndPassword(email, password);
+		const tokens = await tokenService.generateAuthTokens(authuser.id, userAgent);
+	
+		req.user = authuser; // for morgan logger to tokenize it as user
+	
+		res.status(httpStatus.CREATED).send({ user: authuser.filter(), tokens });
+		
+	} catch (error) {
+		throw locateError(error, "AuthController : signup");
+	}
 });
 
 
 
 const login = asyncHandler(async (req, res) => {
-	const { email, password } = req.body;
-	const userAgent = req.useragent.source;
-
-	const authuser = await authService.loginWithEmailAndPassword(email, password);
-	const tokens = await tokenService.generateAuthTokens(authuser.id, userAgent);
-
-	req.user = authuser; // for morgan logger to tokenize it as user
-
-	res.status(httpStatus.OK).send({ user: authuser.filter(), tokens });
+	try {
+		const { email, password } = req.body;
+		const userAgent = req.useragent.source;
+	
+		const authuser = await authService.loginWithEmailAndPassword(email, password);
+		const tokens = await tokenService.generateAuthTokens(authuser.id, userAgent);
+	
+		req.user = authuser; // for morgan logger to tokenize it as user
+	
+		res.status(httpStatus.OK).send({ user: authuser.filter(), tokens });
+		
+	} catch (error) {
+		throw locateError(error, "AuthController : login");
+	}
 });
 
 
 
 const logout = asyncHandler(async (req, res) => {
-	const { id, jti } = req.user; // jti added in the auth middleware
-
-	await authService.logout(id, jti);
-
-	res.status(httpStatus.NO_CONTENT).send();
+	try {
+		const { id, jti } = req.user; // jti added in the auth middleware
+	
+		await authService.logout(id, jti);
+	
+		res.status(httpStatus.NO_CONTENT).send();
+		
+	} catch (error) {
+		throw locateError(error, "AuthController : logout");
+	}
 });
 
 
 
 const signout = asyncHandler(async (req, res) => {
-	const { id, jti } = req.user; // jti added in the auth middleware
-
-	await authService.signout(id, jti);
-
-	res.status(httpStatus.NO_CONTENT).send();
+	try {
+		const { id, jti } = req.user; // jti added in the auth middleware
+	
+		await authService.signout(id, jti);
+	
+		res.status(httpStatus.NO_CONTENT).send();
+		
+	} catch (error) {
+		throw locateError(error, "AuthController : signout");
+	}
 });
 
 
 
 const refreshTokens = asyncHandler(async (req, res) => {
-	const refreshtoken = req.body.refreshToken;
-	const userAgent = req.useragent.source;
-
-	const { authuser, refreshTokenFamily } = await authService.refreshAuth(refreshtoken, userAgent);
-	const tokens = await tokenService.generateAuthTokens(authuser.id, userAgent, refreshTokenFamily);
-
-	req.user = authuser; // for morgan logger to tokenize it as user
+	try {
+		const refreshtoken = req.body.refreshToken;
+		const userAgent = req.useragent.source;
 	
-	res.status(httpStatus.OK).send({ ...tokens });
+		const { authuser, refreshTokenFamily } = await authService.refreshAuth(refreshtoken, userAgent);
+		const tokens = await tokenService.generateAuthTokens(authuser.id, userAgent, refreshTokenFamily);
+	
+		req.user = authuser; // for morgan logger to tokenize it as user
+		
+		res.status(httpStatus.OK).send({ ...tokens });
+		
+	} catch (error) {
+		throw locateError(error, "AuthController : refreshTokens");
+	}
 });
 
 
 
 const forgotPassword = asyncHandler(async (req, res) => {
-	const email = req.body.email;
+	try {
+		const email = req.body.email;
+		
+		const authuser = await authuserService.getAuthUserByEmail(email);
+		const { resetPasswordToken } = await tokenService.generateResetPasswordToken(authuser.id);
+		await emailService.sendResetPasswordEmail(email, resetPasswordToken);
 	
-	const authuser = await authuserService.getAuthUserByEmail(email);
-	const { resetPasswordToken } = await tokenService.generateResetPasswordToken(authuser.id);
-	await emailService.sendResetPasswordEmail(email, resetPasswordToken);
-
-	req.user = authuser; // for morgan logger to tokenize it as user
-
-	res.status(httpStatus.NO_CONTENT).send();
+		req.user = authuser; // for morgan logger to tokenize it as user
+	
+		res.status(httpStatus.NO_CONTENT).send();
+		
+	} catch (error) {
+		throw locateError(error, "AuthController : forgotPassword");
+	}
 });
 
 
 
 const resetPassword = asyncHandler(async (req, res) => {
-	const { token, password } = req.body;
-
-	console.log({ token, password })
-
-	const authuser = await authService.resetPassword(token, password);
-
-	req.user = authuser; // for morgan logger to tokenize it as user
-
-	res.status(httpStatus.NO_CONTENT).send();
+	try {
+		const { token, password } = req.body;
+	
+		console.log({ token, password })
+	
+		const authuser = await authService.resetPassword(token, password);
+	
+		req.user = authuser; // for morgan logger to tokenize it as user
+	
+		res.status(httpStatus.NO_CONTENT).send();
+		
+	} catch (error) {
+		throw locateError(error, "AuthController : resetPassword");
+	}
 });
 
 
 
 const sendVerificationEmail = asyncHandler(async (req, res, next) => {
-	const { id, email, isEmailVerified } = req.user;
-
-	authService.handleEmailIsAlreadyVerified(isEmailVerified);
-	const { verifyEmailToken } = await tokenService.generateVerifyEmailToken(id);
-	await emailService.sendVerificationEmail(email, verifyEmailToken);
-
-	res.status(httpStatus.NO_CONTENT).send();
+	try {
+		const { id, email, isEmailVerified } = req.user;
+	
+		authService.handleEmailIsAlreadyVerified(isEmailVerified);
+		const { verifyEmailToken } = await tokenService.generateVerifyEmailToken(id);
+		await emailService.sendVerificationEmail(email, verifyEmailToken);
+	
+		res.status(httpStatus.NO_CONTENT).send();
+		
+	} catch (error) {
+		throw locateError(error, "AuthController : sendVerificationEmail");
+	}
 });
 
 
 
 const verifyEmail = asyncHandler(async (req, res) => {
-	const token = req.body.token;
-
-	const authuser = await authService.verifyEmail(token);
-
-	req.user = authuser; // for morgan logger to tokenize it as user
-
-	res.status(httpStatus.NO_CONTENT).send();
+	try {
+		const token = req.body.token;
+	
+		const authuser = await authService.verifyEmail(token);
+	
+		req.user = authuser; // for morgan logger to tokenize it as user
+	
+		res.status(httpStatus.NO_CONTENT).send();
+		
+	} catch (error) {
+		throw locateError(error, "AuthController : verifyEmail");
+	}
 });
 
 
 const oAuth = asyncHandler(async (req, res) => {
-	const { id, email } = req.oAuth.user;
-	const oAuthProvider = req.oAuth.provider;
-	const userAgent = req.useragent.source;
-
-	const authuser = await authService.loginWith_oAuth(oAuthProvider, id, email);
-	const tokens = await tokenService.generateAuthTokens(authuser.id, userAgent);
-
-	res.status(httpStatus.OK).send({ user: authuser.filter(), tokens });
+	try {
+		const { id, email } = req.oAuth.user;
+		const oAuthProvider = req.oAuth.provider;
+		const userAgent = req.useragent.source;
+	
+		const authuser = await authService.loginWith_oAuth(oAuthProvider, id, email);
+		const tokens = await tokenService.generateAuthTokens(authuser.id, userAgent);
+	
+		res.status(httpStatus.OK).send({ user: authuser.filter(), tokens });
+		
+	} catch (error) {
+		throw locateError(error, "AuthController : oAuth");
+	}
 });
 
 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,84 +1,120 @@
 const httpStatus = require('http-status');
 const asyncHandler = require('express-async-handler');
 
+const { locateError } = require('../utils/ApiError');
+
 // SERVICE DEPENDENCY
-const { 
-	userService,
-	userDbService,
-	joinedService,
+const {
+	userDbService,		// addUser, getUser, getUsers, updateUser, deleteUser, changeRole
+	userService,		// getUsers
+	joinedService,		// getUsersJoined
 } = require('../services');
 
 
 
 const addUser = asyncHandler(async (req, res) => {
-	const {id, ...addBody} = req.body;
-
-	const user = await userDbService.addUser(id, addBody);
-
-	res.status(httpStatus.CREATED).send(user.filter());
+	try {
+		const {id, ...addBody} = req.body;
+	
+		const user = await userDbService.addUser(id, addBody);
+	
+		res.status(httpStatus.CREATED).send(user.filter());
+		
+	} catch (error) {
+		throw locateError(error, "UserController : addUser");
+	}
 });
 
 
 
 const getUser = asyncHandler(async (req, res) => {
-	const id = req.params.id
-
-	const user = await userDbService.getUser(id);
+	try {
+		const id = req.params.id
 	
-	res.status(httpStatus.OK).send(user.filter());
+		const user = await userDbService.getUser(id);
+		
+		res.status(httpStatus.OK).send(user.filter());
+		
+	} catch (error) {
+		throw locateError(error, "UserController : getUser");
+	}
 });
 
 
 
 const getUsers = asyncHandler(async (req, res) => {
-
-	const query = req.query;
-
-	const result = await userService.getUsers(query);
+	try {
+		const query = req.query;
 	
-	res.status(httpStatus.OK).send(result);
+		const result = await userService.getUsers(query);
+		
+		res.status(httpStatus.OK).send(result);
+		
+	} catch (error) {
+		throw locateError(error, "UserController : getUsers");
+	}
 });
 
 
 
 const getUsersJoined = asyncHandler(async (req, res) => {
-	const query = req.query;
-
-	const result = await joinedService.getUsersJoined(query);
+	try {
+		const query = req.query;
 	
-	res.status(httpStatus.OK).send(result);
+		const result = await joinedService.getUsersJoined(query);
+		
+		res.status(httpStatus.OK).send(result);
+		
+	} catch (error) {
+		throw locateError(error, "UserController : getUsersJoined");
+	}
 });
 
 
 
 const updateUser = asyncHandler(async (req, res) => {
-	const id = req.params.id;
-  	const {name, gender, country} = req.body;
-
-  	const user = await userDbService.updateUser(id, {name, gender, country});
-
-  	res.status(httpStatus.OK).send(user.filter());
+	try {
+		const id = req.params.id;
+		  const {name, gender, country} = req.body;
+	
+		  const user = await userDbService.updateUser(id, {name, gender, country});
+	
+		  res.status(httpStatus.OK).send(user.filter());
+		
+	} catch (error) {
+		throw locateError(error, "UserController : updateUser");
+	}
 });
 
 
 
 const deleteUser = asyncHandler(async (req, res) => {
-	const id = req.params.id;
-
-	await userDbService.deleteUser(id);
-
-	res.status(httpStatus.NO_CONTENT).send();
+	try {
+		const id = req.params.id;
+	
+		await userDbService.deleteUser(id);
+	
+		res.status(httpStatus.NO_CONTENT).send();
+		
+	} catch (error) {
+		throw locateError(error, "UserController : deleteUser");
+	}
 });
 
 
 
 const changeRole = asyncHandler(async (req, res) => {
-	const id = req.params.id;
-	const role = req.body.role
-
-	await userDbService.updateUser(id, {role});
-
-	res.status(httpStatus.NO_CONTENT).send();
+	try {
+		const id = req.params.id;
+		const role = req.body.role
+	
+		await userDbService.updateUser(id, {role});
+	
+		res.status(httpStatus.NO_CONTENT).send();
+		
+	} catch (error) {
+		throw locateError(error, "UserController : changeRole");
+	}
 });
 
 
