2501ed92014e32790f636ac3a48a2f5ef8503366
talatkuyuk <talatkuyuk@gmail.com>
2021-10-24 14:21:47 +0300

    seperated auth middleware into authenticate and authorize middlewares

---
+++ src/middlewares/authenticate.js
@@ -0,0 +1,59 @@
+const passport = require('passport');
+const httpStatus = require('http-status');
+
+const { ApiError, locateError } = require('../utils/ApiError');
+const { redisService } = require('../services');
+
+
+const verifyCallback = (req, resolve, reject, requiredRights) => async (err, pass, info) => {
+	try {
+		if (err || info) {
+			throw new ApiError(httpStatus.UNAUTHORIZED, err || info );
+		}
+		
+		const { authuser, payload } = pass;
+	
+		if (!authuser) {
+			throw new ApiError(httpStatus.UNAUTHORIZED, "Access token does not refer any user");
+		}
+	
+		if (authuser.isDisabled) {
+			throw new ApiError(httpStatus.FORBIDDEN, `You are disabled, call the system administrator`);
+		}
+	
+		// control if the request is coming from the same useragent - for preventing mitm
+		if (req.useragent.source !== payload.ua) {
+			throw new ApiError(httpStatus.UNAUTHORIZED, `Your browser/agent seems changed or updated, you have to re-login to get authentication`);
+		}
+	
+		// control if the token is in the blacklist
+		if (await redisService.check_jti_in_blacklist(payload.jti)) {
+			throw new ApiError(httpStatus.FORBIDDEN, `Access token is in the blacklist`);
+		}
+			
+		authuser.jti = payload.jti; // it refers to access token and refresh token paired
+		req.authuser = authuser;
+		
+		// to get accessToken use req.headers.authorization.split(' ')[1];
+	
+		resolve();
+		
+	} catch (error) {
+		reject( locateError(error, "AuthenticateMiddleware : verifyCallback") );
+	}
+};
+
+const authenticate = async (req, res, next) => {
+	try {
+		await new Promise((resolve, reject) => {
+			passport.authenticate('jwt', { session: false }, verifyCallback(req, resolve, reject))(req, res, next);
+		})
+		
+		next();
+		
+	} catch (error) {
+		next( locateError(error, "AuthenticateMiddleware : authenticate") );
+	}
+};
+
+module.exports = authenticate;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router({ strict: true }); // to handle /joined/ path
 
-const { auth } = require('../middlewares/auth');
-const validate = require('../middlewares/validate');
+const { authenticate, authorize, validate } = require('../middlewares');
 
 const { userController } = require('../controllers');
+
 const userValidation = require('../validations/user.ValidationRules');
 
 
-router.get('/', auth('query-users'), validate(userValidation.getUsers), userController.getUsers);
-router.get('/joined', auth('query-users-joined'), validate(userValidation.getUsers), userController.getUsersJoined);
-router.post('/', auth('add-user'), validate(userValidation.addUser), userController.addUser);
-router.get('/:id', auth('get-user'), validate(userValidation.getUser), userController.getUser);
-router.patch('/:id', auth('update-user'), validate(userValidation.updateUser), userController.updateUser);
-router.put('/:id', auth('change-role'), validate(userValidation.changeRole), userController.changeRole);
-router.delete('/:id', auth('delete-user'), validate(userValidation.deleteUser), userController.deleteUser);
+router.get('/', authenticate, authorize('query-users'), validate(userValidation.getUsers), userController.getUsers);
+router.get('/joined', authenticate, authorize('query-users-joined'), validate(userValidation.getUsers), userController.getUsersJoined);
+router.post('/', authenticate, authorize('add-user'), validate(userValidation.addUser), userController.addUser);
+router.get('/:id', authenticate, authorize('get-user'), validate(userValidation.getUser), userController.getUser);
+router.patch('/:id', authenticate, authorize('update-user'), validate(userValidation.updateUser), userController.updateUser);
+router.put('/:id', authenticate, authorize('change-role'), validate(userValidation.changeRole), userController.changeRole);
+router.delete('/:id', authenticate, authorize('delete-user'), validate(userValidation.deleteUser), userController.deleteUser);
 
 
 module.exports = router;
