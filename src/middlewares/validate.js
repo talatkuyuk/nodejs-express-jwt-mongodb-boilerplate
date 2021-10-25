5981b6cfa77e92ad2ab39f4cc13248f330f413f1
“talatkuyuk” <talatkuyuk@gmail.com>
2021-10-25 17:40:24 +0300

    refined the validate middleware

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,36 +1,35 @@
 const httpStatus = require('http-status');
-const { validationResult } = require('express-validator')
 
+const { validationResult } = require('express-validator')
 const { ApiError, locateError } = require('../utils/ApiError');
 
-const validate = (rulesSchema) => async (req, res, next) => {
+
+const validate = (rules) => async (req, res, next) => {
 	try {
-		// it validates all related validation rules (express-validator rules in validations directory)
-		await Promise.all(rulesSchema.map((rule) => rule.run(req)));
+		// it validates all related validation rules
+		await Promise.all(rules.map((rule) => rule.run(req)));
 
 		const errors = validationResult(req);
 
 		if (errors.isEmpty()) return next();
 
-		const xerrors = {};
+		const myerrors = {};
 
-		// convert errors object to xerrors object as structured below commented.
+		// convert errors object to myerrors object as structured below at the end of the file.
 		errors.array().map(err => {
-			const param = err.param !== "" ? err.param : "body";
-			xerrors[param] = xerrors[param] ? [...xerrors[param], err.msg] : [err.msg];
+			// oneOf([check(...).exists(), ...]) --> param: "_error" (see at users validation)
+			if (err.param === "_error") err.param = "body";
+			
+			const key = err.param || err.location;
+			myerrors[key] = myerrors[key] ? [...myerrors[key], err.msg] : [err.msg];
 		});
 
-		// instead of sending errors directly to the client, I use error handling mechanism below with next function
-		// return res.status(422).json({
-		// 	errors: xerrors
-		// })
-
 		const validationError = new ApiError(
-			httpStatus.UNPROCESSABLE_ENTITY, // statusCode: 422
-			"ValidationError: The request could not be validated", // message
-			null, // no need error description
-			xerrors, // converted validation errors
-			true, // isOperational
+			httpStatus.UNPROCESSABLE_ENTITY, // error.statusCode (422)
+			"ValidationError: The request could not be validated", // error.name: error.message
+			null, // error.description (no need)
+			myerrors, // converted validation errors
+			true, // error.isOperational
 		);
 
 		next(validationError);
@@ -56,7 +55,7 @@ module.exports = validate;
 
 
 // {
-//     "errors": {
+//     "myerrors": {
 //         "param1": [
 //             "message"
 //         ],
