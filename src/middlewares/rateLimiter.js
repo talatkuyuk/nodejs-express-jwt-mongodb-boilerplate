98cf1440dd1247a0905bf394e3980dd37e259b69
talatkuyuk <talatkuyuk@gmail.com>
2022-01-03 21:29:24 +0300

    updated rate-limit middleware

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,9 +1,11 @@
 const rateLimit = require('express-rate-limit');
 
 const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
+  windowMs: 15 * 60 * 1000, // 15 minutes, the duration of a window (in milliseconds)
   max: 20,
-  skipSuccessfulRequests: true,
+  skipSuccessfulRequests: true, // 
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
 module.exports = authLimiter
