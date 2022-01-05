09db1fd92241cb64efc318bbccb8c58a8ca90699
talatkuyuk <talatkuyuk@gmail.com>
2022-01-05 13:46:23 +0300

    fixed a test bug for oAuth routes when there is no internet connection

--- tests/testutils/TestUtil.js
+++ tests/testutils/TestUtil.js
@@ -42,6 +42,25 @@ class TestUtil {
 		},
 	});
 
+	static CheckOneOf = () => expect.extend({
+		toBeOneOf(received, argument) {
+		  const validValues = Array.isArray(argument) ? argument : [argument];
+		  const pass = validValues.includes(received);
+		  if (pass) {
+			return {
+			  message: () => (
+				`expected ${received} not to be one of [${validValues.join(', ')}]`
+			  ),
+			  pass: true,
+			};
+		  }
+		  return {
+			message: () => (`expected ${received} to be one of [${validValues.join(', ')}]`),
+			pass: false,
+		  };
+		},
+	});
+
 
 	static CheckTokenConsistency = (tokens, id) => {
 		const accessToken = tokens.access.token;
